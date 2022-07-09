c49a11519f1ec1965bc3e8ef36e7133767553180
Kamal Dixit <kamalitexpert@gmail.com>
2022-07-09 06:26:59 +0530

    updated dc version

--- ubuntu-docker-compose.sh
+++ ubuntu-docker-compose.sh
@@ -19,7 +19,7 @@ sudo apt-get install docker-ce -y
 sudo docker run hello-world
 
 #Installing docker compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 #Setting required permissions for docker compose
 sudo chmod +x /usr/local/bin/docker-compose
